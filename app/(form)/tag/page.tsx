7076f23e619497768e53b8f4fc29f86a0837ecdb
nishizawayuya <22aw0128@jec.ac.jp>
2023-12-01 14:51:10 +0900

    タグの処理の追加

--- app/(form)/tag/page.tsx
+++ app/(form)/tag/page.tsx
@@ -1,10 +1,23 @@
 'use client'
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 const Tag = ()=>{
 
     const router = useRouter();
     const tagarr:string[]=["すごい","かっこいい","神秘的","可愛い","きれい","大根おろし","鰹節"];
+    // タグ用
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+    const toggleTag = (tag: string) => {
+        if (selectedTags.includes(tag)) {
+          setSelectedTags(selectedTags.filter((selectedTag) => selectedTag !== tag));
+        } else {
+          setSelectedTags([...selectedTags, tag]);
+        }
+    };
+
+
     return(
         <div>
             <button 
@@ -17,11 +30,12 @@ const Tag = ()=>{
             </button>
             <section>
                 <h2 className="text-xl font-bold text-center mt-6">学生に合うタグを選んでください</h2>
-                <div className="flex flex-wrap w-[60%] mx-auto mt-9">
+                <div className="flex flex-wrap w-[80%] mx-auto mt-9 gap-5">
                     {
                         tagarr.map((deta:string,index:number)=>(
-                            <p key={index} className="w-[50%]" onClick={()=>{
-                                console.log(deta);
+                            <p key={index} className={`w-[45%] border border-[#000000] text-center p-3 cursor-pointer ${selectedTags.includes(deta) ? 'bg-gray-300' : ''}`}
+                            onClick={()=>{
+                                toggleTag(deta);
                             }}>#{deta}</p>
                         ))
                     }
@@ -31,6 +45,7 @@ const Tag = ()=>{
                     <button
                         className="bg-[#00ff00] px-8 my-16 rounded-md flex items-center"
                         onClick={()=>{
+                            console.log(selectedTags);
                             router.push("/finish")
                         }}
                     >
