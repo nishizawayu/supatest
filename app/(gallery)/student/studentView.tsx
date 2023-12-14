9515153f96b89d4a868c1082b4498a2f5fc86e45
yuuki0719 <yuuki20030719@gmail.com>
2023-12-15 02:39:28 +0900

    並び替え

--- app/(gallery)/student/studentView.tsx
+++ app/(gallery)/student/studentView.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { useMemo, useState } from "react"
 
 interface StudentViewProps {
-    studentarr: {id:number, uid:string, name:string}[]
+    studentarr: {id:number, uid:string, name:string, job:string, team:string}[]
 }
 
 // 描画用
@@ -12,9 +12,20 @@ const StudentView: React.FC<StudentViewProps> = ({ studentarr }) => {
     const currentData = useMemo(() => {
         if(studentStatus === 1) {
             return studentarr.filter((v) => v.id <=35)
-        } else if(studentStatus === 2) {
+        } 
+        else if(studentStatus === 2) {
             return studentarr.filter((v) => v.id >35)
-        } else {
+        } 
+        else if(studentStatus === 3) {
+            return studentarr.filter((v) => v.job == "エンジニア")
+        }
+        else if(studentStatus === 4) {
+            return studentarr.filter((v) => v.job == "デザイナー")
+        }
+        else if(studentStatus === 5) {
+            return studentarr.filter((v) => v.team == "Bチーム")
+        }
+        else {
             return studentarr
         }
     }, [studentStatus])
@@ -28,6 +39,9 @@ const StudentView: React.FC<StudentViewProps> = ({ studentarr }) => {
                 <li className={`w-[33%] ${studentStatus === 0 ? "bg-sky-200" : ""}`} onClick={() => setStudentStatus(0)}>全体</li>
                 <li className={`w-[33%] ${studentStatus === 1 ? "bg-sky-200" : ""}`} onClick={() => setStudentStatus(1)}>1年生</li>
                 <li className={`w-[33%] ${studentStatus === 2 ? "bg-sky-200" : ""}`}  onClick={() => setStudentStatus(2)}>2年生</li>
+                <li className={`w-[33%] ${studentStatus === 3 ? "bg-sky-200" : ""}`}  onClick={() => setStudentStatus(3)}>エンジニア</li>
+                <li className={`w-[33%] ${studentStatus === 4 ? "bg-sky-200" : ""}`}  onClick={() => setStudentStatus(4)}>デザイナー</li>
+                <li className={`w-[33%] ${studentStatus === 5 ? "bg-sky-200" : ""}`}  onClick={() => setStudentStatus(5)}>Bチーム</li>
             </ul>
             <div className='pb-[120px]'>
                 <ul>
--- app/(gallery)/studentsdata/page.tsx
+++ app/(gallery)/studentsdata/page.tsx
@@ -5,7 +5,7 @@ import { cookies } from 'next/headers';
 const getStudendata = async() =>{
     const cookieStore = cookies()
     const supabase = createClient(cookieStore);
-    const { data: students } = await supabase.from("students").select("*").order('id', { ascending: true });
+    const { data: students } = await supabase.from("students").select("*").order('total_evaluation_score', { ascending: false }).order('id', { ascending: true });
     if(students != null){
         console.log(students);
         return students;
