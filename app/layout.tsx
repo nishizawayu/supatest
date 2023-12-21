2baec011650efa7cb0d1cb70ef16c0638917975d
yuuki0719 <yuuki20030719@gmail.com>
2023-12-21 20:14:57 +0900

    フォント＆背景色変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import { GeistSans } from 'geist/font/sans'
 import './globals.css'
+import { Noto_Sans_JP } from "next/font/google";
+
+const NotoSansJPFont = Noto_Sans_JP({ weight: "400", subsets: ["latin"] });
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -17,9 +20,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="ja" className="bg-[#fafafa]" >
       <body className="bg-background text-foreground">
-        <main>
+        <main className={NotoSansJPFont.className}>
           {children}
         </main>
       </body>
