898b74395e8079e4b0a42be33be19c187f1f4b01
nishizawayuya <22aw0128@jec.ac.jp>
2023-12-31 02:35:51 +0900

    sliderの実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Students from './(gallery)/student/page'
-import Yell from './(omotenasi)/yell/page'
-import Thankyou from './(omotenasi)/thankyou/page'
 import TestImage from './TestImage/page'
+import Slider from './Slider/page'
 
 export default async function Index() {
   // TestImage("バケモノ");
   return (
     <div>
-        <Students/>
+        <Slider/>
     </div>
   )
 }
