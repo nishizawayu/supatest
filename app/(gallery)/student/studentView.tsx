54484bc9ec48e75bd7f85b6494428a7411b66f27
nishizawayuya <22aw0128@jec.ac.jp>
2024-01-02 01:08:44 +0900

    検索機能実装

--- app/(gallery)/student/studentView.tsx
+++ app/(gallery)/student/studentView.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link"
 import TestImage from "@/app/(Slider)/TestImage/page"
-import { useMemo, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 interface StudentViewProps {
     studentarr: {id:number, uid:string, name:string, job:string, team:string, school_year:string, most_common_tag:string}[]
@@ -13,6 +13,7 @@ const StudentView: React.FC<StudentViewProps> = ({ studentarr }) => {
     const [studentYear, setStudentYear] = useState(0)
     const [studentJob, setStudentJob] = useState(0)
     const [studentCharacter, setStudentCharacter] = useState(0)
+    const [studentserch,setStudentserch] = useState("");
     const currentData = useMemo(() => {
         if(studentYear === 1) {
             return studentarr.filter((v) => v.id <=35)
@@ -32,10 +33,25 @@ const StudentView: React.FC<StudentViewProps> = ({ studentarr }) => {
         else if(studentCharacter === 6) {
             return studentarr.filter((v) => v.most_common_tag == "面白い")
         }
+        else if(studentserch >= "0101" || studentserch >= "0201"){
+            return studentarr.filter((v)=> v.uid == studentserch)
+        }
         else {
             return studentarr
         }
-    }, [studentJob,studentYear,studentCharacter])
+        
+    }, [studentJob,studentYear,studentCharacter,studentserch])
+
+    const handleInputChange = (event:any) => {
+        const value = event.target.value
+        if (value.length > 4) {
+            event.target.value = value.slice(0, 4); // 4桁を超える入力を切り捨て
+        }
+        setStudentserch(event.target.value);
+    };
+
+    
+
     return (
         <>
             <h1 className="text-xl font-bold text-center mt-7">
@@ -43,7 +59,7 @@ const StudentView: React.FC<StudentViewProps> = ({ studentarr }) => {
             </h1>
 
             <div className="join flex justify-center mt-8">
-                <input className="input border-black join-item w-[282px]" placeholder="番号で探す"/>
+                <input className="input border-black join-item w-[282px]" type="number" placeholder="番号で探す" value={studentserch} onChange={handleInputChange}/>
                 <button className="btn join-item w-[63px] bg-black text-white border-black">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5q0-1.875-1.312-3.187T9.5 5Q7.625 5 6.313 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14"/></svg>
                 </button>
