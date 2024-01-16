d2761b54facc31d9d85d2e386ab3bec550633eea
yuuki0719 <yuuki20030719@gmail.com>
2024-01-16 10:42:56 +0900

    初期画面変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Students from './(gallery)/student/page'
 import TeamRank from './(Slider)/teamrank/page'
+import FormFirst from './(form)/first/page'
 
 export default async function Index() {
   return (
     <div>
-        <Students/>
+        <FormFirst/>
     </div>
   )
 }
