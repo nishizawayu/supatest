71db8eea1f0c71287bc316125533870eaf3353fc
yuuki0719 <yuuki20030719@gmail.com>
2024-01-17 12:37:49 +0900

    loading追加

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+
+
+const Loading=()=> {
+    return (
+        <div className="sk-chase w-full mt-[40%] mx-auto">
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+        </div>
+    )
+  }
+
+export default Loading;
