5b314f675814b21ae91b3d6e5ebe65d18d28aa2d
yuuki0719 <yuuki20030719@gmail.com>
2024-01-17 12:38:11 +0900

    日付のフォーマット変換

--- app/(Slider)/imagedata/page.tsx
+++ app/(Slider)/imagedata/page.tsx
@@ -7,10 +7,22 @@ const getImagedata = async() =>{
     const supabase = createClient(cookieStore);
 
     const { data: students } = await supabase.from("teamsimageurl").select("*").order('id', { ascending: true });
-    if(students != null){
-        console.log(students);
-        return students;
+    
+    if (students != null) {
+        const formattedStudents = students?.map(student => {
+            return {
+                ...student,
+                created_at: formatDate(student.created_at)
+            };
+        });
+
+        console.log(formattedStudents);
+        return formattedStudents;
     }
+}
+
+function formatDate(dateString:any) {
+    return new Date(dateString).toLocaleString('ja-JP', { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
 }
 
 export default getImagedata
