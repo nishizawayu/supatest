d8bad55b9a10fa0478489d4431eeec33f8aa80d1
yuuki0719 <yuuki20030719@gmail.com>
2024-01-22 12:28:47 +0900

    色々

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Students from './(gallery)/student/page'
 import TeamRank from './(Slider)/teamrank/page'
 import FormFirst from './(form)/first/page'
+import { Suspense } from 'react'
+import Loading from "@/app/loading";
 
 export default async function Index() {
   return (
     <div>
+      <Suspense fallback={<Loading />}>
         <FormFirst/>
+      </Suspense>
     </div>
   )
 }
