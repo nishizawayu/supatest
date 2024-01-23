4d8a2eed4c8e02e39de1b3e4c32f785b0b88800e
nishizawayuya <22aw0128@jec.ac.jp>
2024-01-24 03:09:22 +0900

    微調整

--- app/TestImage/insert.tsx
+++ app/TestImage/insert.tsx
@@ -3,7 +3,7 @@ import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 
 // サーバー接続用
-const insertimagedata = async(imageUrl:string,tid:number) =>{
+const insertimagedata = async(imageUrl:string,tid:number,tag:[]) =>{
     console.log("inset-imageUrl");
     const cookieStore = cookies()
     const supabase = createClient(cookieStore);
@@ -11,7 +11,8 @@ const insertimagedata = async(imageUrl:string,tid:number) =>{
     .from('teamsimageurl')     
     .insert({
         tid:tid,
-        imageUrl:imageUrl
+        imageUrl:imageUrl,
+        tag:tag
     })
     if(error){
         console.log(error);
